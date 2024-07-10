1c20cc31c0d6525e52c000f148d0a6114621d3ca
faverogian <gian.favero@mail.mcgill.ca>
2024-07-10 01:04:48 -0400

    unconditional trained using UNet2DConditionModel

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 """
-A script for training a diffusion model on the Smithsonian Butterflies dataset following the simpleDiffusion paradigm.
+A script for training a ControlNet on the Smithsonian Butterflies dataset following the simpleDiffusion paradigm.
 
-This script uses the UNet2D model from the diffusion library and the simpleDiffusion model from the simple_diffusion library. 
+An adaptation of the HuggingFace ControlNet training script.
 """
 
-from diffusion.unet import UNet2D, simpleUNet
+from diffusion.unet import UNet2D, simpleUNet, UNetCondition2D
 from diffusion.simple_diffusion import simpleDiffusion
 
 from datasets import load_dataset
@@ -16,13 +16,16 @@ from diffusers.optimization import get_cosine_schedule_with_warmup
 class TrainingConfig:
     image_size = 128  # the generated image resolution
     train_batch_size = 4
-    num_epochs = 200
+    num_epochs = 1000
     gradient_accumulation_steps = 1
     learning_rate = 5e-5
     lr_warmup_steps = 10000
     save_image_epochs = 50
     mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
     output_dir = "ddpm-butterflies-128"  # the model name locally and on the HF Hub
+    push_to_hub = True  # whether to upload the saved model to the HF Hub
+    hub_model_id = "faverogian/Smithsonian128UNet"  # the name of the repository to create on the HF Hub
+    hub_private_repo = False
     overwrite_output_dir = True  # overwrite the old model when re-running the notebook
     seed = 0
 
@@ -56,18 +59,29 @@ def main():
         shuffle=True,
     )
 
-    unet = simpleUNet(
-        img_resolution=config.image_size,
-        in_channels=3,
-        out_channels=3,
-        model_channels=192,
-        channel_mult=(1, 1, 2, 2, 4, 4),
-        num_res_blocks=(2, 2, 2, 2, 2, 2),
-        attention_resolutions=(16, 8),
-        dropout=0.1,
-        dropout_from=16,
-        downsample=1,
-        fp16=True
+    unet = UNetCondition2D(
+        sample_size=config.image_size,  # the target image resolution
+        in_channels=3,  # the number of input channels, 3 for RGB images
+        out_channels=3,  # the number of output channels
+        layers_per_block=2,  # how many ResNet layers to use per UNet block
+        block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channels for each UNet block
+        down_block_types=(
+            "DownBlock2D",
+            "DownBlock2D",
+            "DownBlock2D",
+            "DownBlock2D",
+            "AttnDownBlock2D",
+            "DownBlock2D",
+        ),
+        up_block_types=(
+            "UpBlock2D",
+            "AttnUpBlock2D",
+            "UpBlock2D",
+            "UpBlock2D",
+            "UpBlock2D",
+            "UpBlock2D",
+        ),
+        mid_block_type="UNetMidBlock2D",
     )
 
     optimizer = torch.optim.Adam(unet.parameters(), lr=config.learning_rate)
